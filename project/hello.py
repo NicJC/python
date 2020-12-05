c6d01e204220853b2e849b89a37fa5ced86c8c90
Nicholas Coxen <39826682+NicJC@users.noreply.github.com>
2020-12-05 15:31:53 +1300

    Update hello.py

--- project/hello.py
+++ project/hello.py
@@ -1,19 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 def implode(lst):
    s = ""
    for e in lst:
     s = s+e
    return s
-print(implode(['h', 'e', 'l', 'l', 'o']))
-         
+print(implode(['h', 'e', 'l', 'l', 'o']))         
 
 
-# In[ ]:
 
 
 
