b835038ae6b157ee6b02106a68e4f9e16c1184f2
Nicholas Coxen <39826682+NicJC@users.noreply.github.com>
2020-12-05 15:28:32 +1300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def implode(lst):
+   s = ""
+   for e in lst:
+    s = s+e
+   return s
+print(implode(['h', 'e', 'l', 'l', 'o']))
+         
+
+
+# In[ ]:
+
+
+
+
